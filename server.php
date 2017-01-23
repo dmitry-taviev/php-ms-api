108a24a5501cbd5d48f9505986d2708de96cafc1
Dmitry Taviev <dmitry.taviev@applyit.lv>
2017-01-23 18:30:01 +0200

    moved app server to its own package

--- server.php
+++ server.php
@@ -22,10 +22,6 @@ $app->before(function(\Symfony\Component\HttpFoundation\Request $request) {
 
 $storage = ['persons' => []];
 
-//$app['persons'] = [
-//    ['id' => 0, 'name' => 'Dmitry']
-//];
-
 $app->get('/persons', function() use ($app, &$storage) {
     return $app->json($storage['persons']);
 });
@@ -42,50 +38,5 @@ $app->post('/persons', function(\Symfony\Component\HttpFoundation\Request $reque
 });
 
 // SERVER
-
-$loop = \React\EventLoop\Factory::create();
-$socket = new \React\Socket\Server($loop);
-$server = new \React\Http\Server($socket);
-$server->on('request', function(\React\Http\Request $request, \React\Http\Response $response) use ($app) {
-    $contentLength = (int) $request->getHeaders()['Content-Length'];
-    $dataReceived = '';
-    $totalDataLength = 0;
-    $request->on('data', function(string $data) use (&$dataReceived, &$totalDataLength, $contentLength, $request, $response, $app) {
-        $dataReceived .= $data;
-        $totalDataLength += strlen($data);
-        if ($totalDataLength >= $contentLength) {
-            parse_str($dataReceived, $postParameters);
-            $method = $request->getMethod();
-            $input = \Symfony\Component\HttpFoundation\Request::create(
-                $request->getPath(),
-                $method,
-                ($method === 'GET' ? $request->getQuery() : $postParameters),
-                [],
-                [],
-                [],
-                $dataReceived
-            );
-            $input->headers->replace($request->getHeaders());
-            $output = $app->handle($input);
-            $app->terminate($input, $output);
-            $response->writeHead(
-                $output->getStatusCode(),
-                array_map(
-                    function(array $headerValues) {
-                        return $headerValues[0];
-                    },
-                    $output->headers->all()
-                )
-            );
-            $response->end($output->getContent());
-            $request->close();
-        }
-    });
-
-});
-
-$socket->listen(1337, '0.0.0.0');
-
-echo 'Listening on ' . $socket->getPort() . PHP_EOL;
-
-$loop->run();
+$server = new \AppServer\BasicServer();
+$server->serve($app, 1337);
