412fc778db9c75893fd5ccb5204439c6d8cc60d5
Dmitry Taviev <dmitry.taviev@applyit.lv>
2017-01-30 17:36:35 +0200

    cleanup

--- server.php
+++ server.php
@@ -14,23 +14,7 @@ require __DIR__ . '/vendor/autoload.php';
 $app = new \AppServer\Application();
 $app->mount('/persons', new \Example\PersonRegistry\Component());
 
-//$storage = ['persons' => []];
-//
-//$app->get('/persons', function() use ($app, &$storage) {
-//    return $app->json($storage['persons']);
-//});
-//$app->get('/persons/{id}', function(int $id) use ($app, &$storage) {
-//    return $app->json($storage['persons'][$id - 1]);
-//});
-//$app->post('/persons', function(\Symfony\Component\HttpFoundation\Request $request) use ($app, &$storage) {
-//    $newId = count($storage['persons']);
-//    $storage['persons'][$newId] = [
-//        'id' => $newId + 1,
-//        'name' => $request->request->getAlpha('name')
-//    ];
-//    return $app->json($storage['persons'][$newId], 201);
-//});
-
 // SERVER
-$server = new \AppServer\BasicServer(true);
+$debugging = true;
+$server = new \AppServer\BasicServer($debugging);
 $server->serve($app, 1337);
